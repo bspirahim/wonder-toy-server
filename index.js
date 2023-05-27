25e7d51a89a5adddca8296ee3aed12be77928ab2
Abdur-P-Rahim <bspirahim@gmail.com>
2023-05-27 18:05:18 +0600

    all toys data insert done

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res)=>{
-    res.send('wonder toy is running')
-})
+
 
 console.log(process.env.DB_PASS)
 
@@ -32,19 +30,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const AllToysCollection = client.db("wonderToy").collection("alltoys");
+
+
+   
+
+    app.post('/alltoys', async(req, res)=>{
+      const allToy = req.body;
+      const result = await AllToysCollection.insertOne(allToy)
+      res.send(result);
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+   /*  await client.close(); */
   }
 }
 run().catch(console.dir);
 
 
 
-
+app.get('/', (req, res)=>{
+  res.send('wonder toy is running')
+})
 
 
 app.listen(port, ()=>{

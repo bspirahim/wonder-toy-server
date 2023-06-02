a94297f96cc3bce24e6e4b618f94ae9433db3ae4
Abdur-P-Rahim <bspirahim@gmail.com>
2023-06-03 01:28:47 +0600

    get single toy data with id added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -34,7 +34,26 @@ async function run() {
     const AllToysCollection = client.db("wonderToy").collection("alltoys");
 
 
-   
+    app.get('/altoys', async(req, res)=>{
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email }
+      }
+      const result = await AllToysCollection.find(query).toArray();
+      res.send(result)
+    })
+
+
+    app.get('/altoys/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: {  img: 1, Price: 1, ProductName:1, Quantity:1, SellerName:1, email:1, details:1 },
+      };
+      const result = await AllToysCollection.findOne(query, options)
+      res.send(result)
+    })
 
     app.post('/alltoys', async(req, res)=>{
       const allToy = req.body;
